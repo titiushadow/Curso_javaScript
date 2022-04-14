ffe19a031b2a43d724fcd16bb8de0d1c55fdc7a7
mesaque <mesaque1212@gmail.com>
2022-04-13 21:23:38 -0300

    Criando arquivo script da aula_3

---
+++ 3_ Particularidades_js/js/script.js
@@ -0,0 +1,13 @@
+console.log("Primeiro");
+
+console.log("Ultimo");
+
+// Console.LOG não existe, javascript é case sensivite
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// as vezes o ponto e virgula não é obrigatorio no javascript
