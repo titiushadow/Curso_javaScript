24e9b24c4691019801a8a25e4fdb48151a6fefb6
mesaque <mesaque1212@gmail.com>
2022-04-15 12:34:19 -0300

    null and undefined Hoisting - içamento

---
+++ 9_Tipo_dados_null_undefined/js/script.js
@@ -0,0 +1,15 @@
+// Hoisting - içamento
+console.log(sobrenome);
+
+
+var number = 5;
+console.log(number);
+
+var nome = null;
+var sobrenome = "Moises";
+console.log(nome);
+console.log(sobrenome);
+
+nome = 'Mesaque';
+console.log(nome);
+
