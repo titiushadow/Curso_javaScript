f860cc9dd3c8be31e30526029b295bbed39cff56
mesaque <mesaque1212@gmail.com>
2022-04-15 12:47:02 -0300

    Objetos Como acessar uma propriedade no objeto usando obj.nome como se fosse uma variavel

---
+++ 10_Tipo_dados_Objeto/js/script.js
@@ -0,0 +1,19 @@
+// objetos
+var obj = {
+    nome: "Mesaque",
+    idade: 18,
+    profissão: "Programador"
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+// Como acessar uma propriedade no objeto.
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissão);
+
+// Podemos também usar como se fosse uma variavel como no exemplo abaixo, é so concatenar
+console.log("O meu nome é " + obj.nome);
+console.log("Eu tenho:" + obj.idade);
+console.log("Eu sou um:" + obj.profissão);
