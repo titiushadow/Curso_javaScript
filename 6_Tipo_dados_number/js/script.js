53c45391200a6872e3ceeaabdc3ad907fbcf1212
mesaque <mesaque1212@gmail.com>
2022-04-15 11:41:31 -0300

    Verificando o tipo de dados usando typeof

---
+++ 6_Tipo_dados_number/js/script.js
@@ -0,0 +1,25 @@
+// typeof retorna o tipo da variavel: Number, string, boolean...
+var number = 5;
+console.log(number);
+console.log(typeof number);
+
+
+// typeof também funciona para numeros quebrados 5.25 ou 8.35...
+var float = 5.30;
+console.log(float);
+console.log(typeof float);
+
+
+var TextNumber = '525';
+console.log(TextNumber);
+console.log(typeof TextNumber);
+
+// Valores simbolicos que representam numbers em javascript.
+console.log(NaN)
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
+
+// Os tipos de dados mais usados em javascript são:
+// Number, String, Boolean, null e undefined, Object;
+// Podemos verificar o tipo do dado usando typeof.
