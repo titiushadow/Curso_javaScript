de3f952eadaf215731bef77803b7eec186331979
mesaque <mesaque1212@gmail.com>
2022-04-15 11:21:35 -0300

    Declarando variaveis

---
+++ 5_Variaveis/js/script.js
@@ -0,0 +1,26 @@
+// Declarando a primeira variavel em javascript
+var teste = 2;
+console.log(teste);
+
+
+// Declarando varivael do tipo string
+teste = 'Mesaque';
+console.log(teste);
+
+
+// Os unicos simbolos que pode ter para declarar uma variavel é $ ou _
+var $nome = 'Meseikers'
+console.log($nome);
+
+var _nome = 'Mezikers'
+console.log(_nome);
+
+
+// Juntando dois numeros 
+var $number = '2' + '2';
+console.log($number);
+
+
+// Se a variavel não tiver nada declarado ela volta como undefined.
+var meuNome;
+console.log(meuNome);
