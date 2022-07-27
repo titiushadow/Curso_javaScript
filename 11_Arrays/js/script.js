5068e65e2988d419826aef9bc6987dac08b2a7ec
mesaque <mesaque1212@gmail.com>
2022-07-27 12:02:48 -0300

    Criar arrays Adiconar elementos no array Modificar elementos do array

---
+++ 11_Arrays/js/script.js
@@ -0,0 +1,16 @@
+var arr = ["mesaque", 18, true, {primeiro: 1,
+segundo: 2}]; // objeto => {}
+
+console.log(arr)
+
+var array2 = [1,2,3,4];
+console.log(array2)
+console.log(array2[1]); // acessando elementos do array
+
+// Adicionar elementos no array
+arr[4] = 11;
+console.log(arr)
+
+// Modificar elementos de um indice especifico
+arr[0] = "Javascript";
+console.log(arr)
